c302d5fc7f32d9aae6e0c50e2181f8839e7f03c3
jsaetrum <jsaetrum@hotmail.com>
2015-05-02 20:44:01 +0200

    minor changes

--- Main/source/model/OBJParser.hpp
+++ Main/source/model/OBJParser.hpp
@@ -63,93 +63,90 @@ using objfile::Model;
 
 namespace model
 {
-   //namespace objfile
-   //{
-   //   //struct Model;
-   //   struct Object;
-   //   struct Material;
-   //   class Point3f;
-   //   class Point2f;
-   //}
-   class ObjFileImporter;
-   class IOSystem;
-
-   ///	\class	ObjFileParser
-   ///	\brief	Parser for a obj waveform file
-   class ObjFileParser
-   {
-   public:
-      static const size_t BUFFERSIZE = 4096;
-      typedef std::vector<char> DataArray;
-      typedef std::vector<char>::iterator DataArrayIt;
-      typedef std::vector<char>::const_iterator ConstDataArrayIt;
-
-   public:
-      ///	\brief	Constructor with data array.
-      ObjFileParser(std::vector<char> &Data, const String_c &strModelName, IOSystem* io);
-      ///	\brief	Destructor
-      ~ObjFileParser();
-      ///	\brief	Model getter.
-      objfile::Model *GetModel() const;
-
-   private:
-      ///	Parse the loaded file
-      void parseFile();
-      ///	Method to copy the new delimited word in the current line.
-      void copyNextWord(char *pBuffer, size_t length);
-      ///	Method to copy the new line.
-      void copyNextLine(char *pBuffer, size_t length);
-      /// Stores the vector 
-      void getVector(std::vector<Vector3f> &point3d_array);
-      ///	Stores the following 3d vector.
-      void getVector3(std::vector<Vector3f> &point3d_array);
-      ///	Stores the following 3d vector.
-      void getVector2(std::vector<Vector2f> &point2d_array);
-      ///	Stores the following face.
-      void getFace(aiPrimitiveType type);
-      /// Reads the material description.
-      void getMaterialDesc();
-      ///	Gets a comment.
-      void getComment();
-      /// Gets a a material library.
-      void getMaterialLib();
-      /// Creates a new material.
-      void getNewMaterial();
-      /// Gets the group name from file.
-      void getGroupName();
-      /// Gets the group number from file.
-      void getGroupNumber();
-      /// Gets the group number and resolution from file.
-      void getGroupNumberAndResolution();
-      /// Returns the index of the material. Is -1 if not material was found.
-      int32 getMaterialIndex(const String_c &strMaterialName);
-      /// Parse object name
-      void getObjectName();
-      /// Creates a new object.
-      void createObject(const String_c &strObjectName);
-      ///	Creates a new mesh.
-      void createMesh();
-      ///	Returns true, if a new mesh instance must be created.
-      bool needsNewMesh(const String_c &rMaterialName);
-      ///	Error report in token
-      void reportErrorTokenInFace();
-
-   private:
-      ///	Default material name
-      static const String_c DEFAULT_MATERIAL;
-      //!	Iterator to current position in buffer
-      DataArrayIt m_DataIt;
-      //!	Iterator to end position of buffer
-      DataArrayIt m_DataItEnd;
-      //!	Pointer to model instance
-      objfile::Model *m_pModel;
-      //!	Current line (for debugging)
-      uint32 m_uiLine;
-      //!	Helper buffer
-      char m_buffer[BUFFERSIZE];
-      ///	Pointer to IO system instance.
-      IOSystem *m_pIO;
-   };
+  
+  namespace objparser
+  {
+       //namespace objfile
+       //{
+       //   //struct Model;
+       //   struct Object;
+       //   struct Material;
+       //   class Point3f;
+       //   class Point2f;
+       //}
+       class ObjFileImporter;
+       class IOSystem;
+    
+       ///	\class	ObjFileParser
+       ///	\brief	Parser for a obj waveform file
+       class ObjFileParser
+       {
+       private:
+          static const String_c DEFAULT_MATERIAL_NAME;
+          DataArrayIt m_DataIterator;
+          DataArrayIt m_DataIteratorEndOfBuffer;
+          objfile::Model *m_pModelInstance;
+          uint32 m_uiCurrentLine;
+          //	Helper buffer
+          char m_buffer[BUFFERSIZE];
+          //	Pointer to IO system instance.
+          IOSystem *m_pIO;   
+       public:
+          static const size_t BUFFERSIZE = 4096;
+          typedef std::vector<char> DataArray;
+          typedef std::vector<char>::iterator DataArrayIt;
+          typedef std::vector<char>::const_iterator ConstDataArrayIt;
+    
+          ///	\brief	Constructor with data array.
+          ObjFileParser(std::vector<char> &Data, const String_c &strModelName, IOSystem* io);
+          ///	\brief	Destructor
+          ~ObjFileParser();
+          objfile::Model *GetModel() const
+    
+       private:
+          //	Parse the loaded file
+          void ParseFile();
+          //	Method to copy the new delimited word in the current line.
+          void CopyNextWord(char *pBuffer, size_t length);
+          //	Method to copy the new line.
+          void CopyNextLine(char *pBuffer, size_t length);
+          // Stores the vector 
+          void GetVector(std::vector<Vector3f> &point3d_array);
+          //	Stores the following 3d vector.
+          void GetVector3(std::vector<Vector3f> &point3d_array);
+          //	Stores the following 3d vector.
+          void GetVector2(std::vector<Vector2f> &point2d_array);
+          //	Stores the following face.
+          void GetFace(aiPrimitiveType type);
+          // Reads the material description.
+          void GetMaterialDesc();
+          //	Gets a comment.
+          void GetComment();
+          // Gets a a material library.
+          void GetMaterialLib();
+          // Creates a new material.
+          void GetNewMaterial();
+          // Gets the group name from file.
+          void GetGroupName();
+          // Gets the group number from file.
+          void GetGroupNumber();
+          // Gets the group number and resolution from file.
+          void GetGroupNumberAndResolution();
+          // Returns the index of the material. Is -1 if not material was found.
+          int32 getMaterialIndex(const String_c &strMaterialName);
+          // Parse object name
+          void GetObjectName();
+          // Creates a new object.
+          void CreateObject(const String_c &strObjectName);
+          //	Creates a new mesh.
+          void CreateMesh();
+          //	Returns true, if a new mesh instance must be created.
+          bool NeedsNewMesh(const String_c &rMaterialName);
+          //	Error report in token
+          void ReportErrorTokenInFace();
+      };
+       
+    } // namespace objparser
 
 } //namespace model
 
