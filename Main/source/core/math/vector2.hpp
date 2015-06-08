665a143728c94f298c42cf12d10d009ec5a5d4b1
jsaetrum <jsaetrum@hotmail.com>
2015-06-08 02:52:00 +0000

    Some simplifications made
    Removed: ugly enum ctor, enum, SetUnitX/Y  etc. functions removed.
    Added: some common vector constant.
    You can set common constants by using assignment operators with the constants.

--- Main/source/core/math/vector2.hpp
+++ Main/source/core/math/vector2.hpp
@@ -10,36 +10,23 @@ namespace core
 
    namespace math
    {
-
-      enum eConstructorVector2
-      {
-         VEC2_CONST_NOTHING,
-         VEC2_CONST_ZERO,
-         VEC2_CONST_UNIT_X,
-         VEC2_CONST_UNIT_Y,
-         VEC2_CONST_NEG_UNIT_X,
-         VEC2_CONST_NEG_UNIT_Y,
-         VEC2_CONST_UNIT_SCALE
-      };
-
-      template <class T>
-      class Vector2 : public Point2 < T >
+      template <typename T>
+      class Vector2 : public Point2 <T>
       {
       public:
          using Point2<T>::operator=;
 
+         static const Vector2<T> ZERO;
+         static const Vector2<T> UNIT_X;
+         static const Vector2<T> UNIT_Y;
+         static const Vector2<T> NEG_UNIT_X;
+         static const Vector2<T> NEG_UNIT_Y;
+         static const Vector2<T> NEG_UNIT_SCALE; 
+
          //----------- constructors
          Vector2();
-         Vector2(const eConstructorVector2 constructor);
          Vector2(const T x, const T y);
          Vector2(const T scaler);
-
-         //----------- mutators
-         void SetUnitX();
-         void SetUnitY();
-         void SetNegUnitX();
-         void SetNegUnitY();
-         void SetUnitScale();
 
          T operator*(const Vector2 &other) const;
          T DotProd(const Vector2 &other) const;
@@ -58,109 +45,56 @@ namespace core
       typedef Vector2<double> Vector2d;
       typedef Vector2<long double> Vector2ld; // TODO: replace with new type
 
-      template <class T>
+      template <typename T> const Vector2<T> Vector2<T>::ZERO(0, 0);
+      template <typename T> const Vector2<T> Vector2<T>::UNIT_X(1, 0);
+      template <typename T> const Vector2<T> Vector2<T>::UNIT_Y(0, 1);
+      template <typename T> const Vector2<T> Vector2<T>::NEG_UNIT_X(-1, 0);
+      template <typename T> const Vector2<T> Vector2<T>::NEG_UNIT_Y(0, -1);
+      template <typename T> const Vector2<T> Vector2<T>::NEG_UNIT_SCALE(1, 1);
+
+      template <typename T>
       inline Vector2<T>::Vector2()
       {
       }
 
-      template <class T>
-      inline Vector2<T>::Vector2(const eConstructorVector2 constructor)
-      {
-         switch (eConstructorVector2)
-         {
-         case CONST_NOTHING:
-            break;
-         case CONST_ZERO:
-            Zero();
-            break;
-         case CONST_UNIT_X:
-            SetUnitX();
-            break;
-         case CONST_UNIT_Y:
-            SetUnitY();
-            break;
-         case CONST_NEG_UNIT_X:
-            SetNegUnitX();
-            break;
-         case CONST_NEG_UNIT_Y:
-            SetNegUnitY();
-            break;
-         case CONST_UNIT_SCALE:
-            SetUnitScale();
-            break;
-         default:
-            break;
-         }
-      }
-
-      template <class T>
+      template <typename T>
       inline Vector2<T>::Vector2(const T x, const T y)
       {
          this->x = x;
          this->y = y;
       }
 
-      template <class T>
-      inline void Vector2<T>::SetUnitX()
-      {
-         x = (T)1; y = (T)0;
-      }
-
-      template <class T>
-      inline void Vector2<T>::SetUnitY()
-      {
-         x = (T)0; y = (T)1;
-      }
-
-      template <class T>
-      inline void Vector2<T>::SetNegUnitX()
-      {
-         x = (T)-1; y = (T)0;
-      }
-
-      template <class T>
-      inline void Vector2<T>::SetNegUnitY()
-      {
-         x = (T)0; y = (T)-1;
-      }
-
-      template <class T>
-      inline void Vector2<T>::SetUnitScale()
-      {
-         x = (T)1; y = (T)1;
-      }
-
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::operator*(const Vector2<T> &other) const
       {
          return x * other.x + y * other.y;
       }
 
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::DotProd(const Vector2<T> &other) const
       {
          return x * other.x + y * other.y;
       }
 
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::CrossProd(const Vector2<T> &other) const
       {
          return x * other.y - y * other.x;
       }
 
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::SqLength() const
       {
          return x * x + y * y;
       }
 
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::Length() const
       {
          return sqrt(x * x + y * y);
       }
 
-      template <class T>
+      template <typename T>
       inline T Vector2<T>::Normalize()
       {
          float length;
@@ -177,7 +111,7 @@ namespace core
          return length;
       }
 
-      template <class T>
+      template <typename T>
       inline Vector2<T> Vector2<T>::CreatePerpendicularVec() const
       {
          return Vector2<T>(-y, x);
