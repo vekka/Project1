a966fdd247e9cc5336aa2e45a0f31e0f6e7d681b
jsaetrum <jsaetrum@hotmail.com>
2015-06-18 21:59:32 +0200

    added class OALDriver

--- Main/source/openal/oaldriver.hpp
+++ Main/source/openal/oaldriver.hpp
@@ -1,4 +1,21 @@
 #ifndef _OALDRIVER_HPP_INCLUDED_
 #define _OALDRIVER_HPP_INCLUDED_
 
+#include <AL/al.h>
+#include <AL/alc.h>
+
+namespace oaldriver
+{
+   class OALDriver
+   {
+   private:
+       ALCdevice *m_device;
+       ALCcontext *m_context;
+   public:
+       OALDriver() : m_device(NULL), m_context(NULL) {}
+       ~OALDriver() {}
+       bool CreateContext();
+   };
+} // namespace oaldriver
+
 #endif
