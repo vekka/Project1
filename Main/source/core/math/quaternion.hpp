8d9d1fe857906c2838b0d36b2ed87653e7a180d8
jsaetrum <jsaetrum@hotmail.com>
2015-06-19 21:10:17 +0200

    moved Inverse() to quaternion.cpp

--- Main/source/core/math/quaternion.hpp
+++ Main/source/core/math/quaternion.hpp
@@ -263,33 +263,6 @@ namespace core
 	            return q1.x*q2.x + q1.y*q2.y + q1.z*q2.z + q1.w*q2.w;
 	        }
 	        
-	        // should not be inlined
-	        template <class T>
-		inline Quaternion<T> Quaternion<T>::Inverse(void)
-		{
-		   float magnitude = x*x + y*y + z*z + w*w;
-		   assert(!core::IsZero(magnitude));
-		   float invMagnitude;
-		   if( core::Equals(magnitude, T(1)) )    // special case: unit quaternion
-		   {
-		      x = -x;
-		      y = -y;
-		      z = -z;
-		   }
-		   else // else scale
-		   {
-		      if( core::IsZero(magnitude) )
-		         invMagnitude = T(1);
-		      else
-		         invMagnitude = T(1) / magnitude;
-		      x *= -invMagnitude;
-		      y *= -invMagnitude;
-		      z *= -invMagnitude;
-		      w *= invMagnitude;
-		   }
-		   return *this;
-		}        
-	        
 	        template <class T>
 	        T Quaternion<T>::Magnitude(void) const
 	        {
