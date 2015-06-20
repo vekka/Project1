e13e1bd880a07a7fe21dd3643ab8aaad1f98c172
jsaetrum <jsaetrum@hotmail.com>
2015-06-20 21:50:32 +0200

    Update oaldriver.cpp

--- Main/source/openal/oaldriver.cpp
+++ Main/source/openal/oaldriver.cpp
@@ -1,19 +1,22 @@
 #include "oaldriver.hpp"
 
-namespace oaldriver
+namespace sound
 {
-   bool OALDriver::Init()
+   namespace oaldriver
    {
-      m_pDevice = alcOpenDevice(NULL);
-      if (!m_pDevice)
-         return false;
-      m_pContext = alcCreateContext(m_pDevice, NULL);
-      alcMakeContextCurrent(m_pContext);
-      if (!m_pContext)
-         return false;
-      alGenBuffers(NUM_BUFFERS, buffers);
-      alGenSources(1, &source);
-      if (alGetError() != AL_NO_ERROR)
-         return false;
-   }
-} // namespace oaldriver
+      bool OALDriver::Init()
+      {
+         m_pDevice = alcOpenDevice(NULL);
+         if (!m_pDevice)
+            return false;
+         m_pContext = alcCreateContext(m_pDevice, NULL);
+         alcMakeContextCurrent(m_pContext);
+         if (!m_pContext)
+            return false;
+         alGenBuffers(NUM_BUFFERS, buffers);
+         alGenSources(1, &source);
+         if (alGetError() != AL_NO_ERROR)
+            return false;
+      }
+   } // namespace oaldriver
+} // sound
