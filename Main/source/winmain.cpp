ffcaf32495c5497daa77a2486d4ff2ce75ab1a29
jsaetrum <jsaetrum@hotmail.com>
2015-06-26 21:42:47 +0200

    added some q3-arena inspired code

--- Main/source/winmain.cpp
+++ Main/source/winmain.cpp
@@ -1,7 +1,33 @@
+#include "core/BasicTypes.hpp"
+
+#include "win32/Win32Console.hpp"
+using win32console::Win32Console;
+
+#include "win32/Win32Timer.hpp"
+using win32timer::Win32Timer;
 
 int32 WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int32 nCmdShow)
 {
+   Win32Console sysConsole;
+      
+   // for error output
+   sysConsole.Create();
+      
+   Win32Timer win32Timer;
+   win32Timer.GetMilliSecs();
+   
    while(1)
    {
+      int32 startTime, endTime;
+      int32 totalMS, countMS;
+      
+      startTime = Sys_Milliseconds();
+      
+      //InFrame();
+      
+      //ComFrame();
+      
+   	totalMS += endTime - startTime;
+		countMS++;   
    }
 }
