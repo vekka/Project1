8c9b1b354e2b095459d38ed1af0772483553d7f1
jsaetrum <jsaetrum@hotmail.com>
2015-06-26 21:45:10 +0200

    split into two classes: Win32Timer and hires Win32Timer64

--- Main/source/win32/win32timer.hpp
+++ Main/source/win32/win32timer.hpp
@@ -5,39 +5,45 @@
 
 namespace win32timer
 {
+
+   class Win32Timer
+   {
+   private:
+      static bool m_initialized;
+      uint32 m_timeBase;
+      
+   public:
+       Win32Timer();
    
-// high-resolution timer (64-bit)
-class Win32Timer
-{
-private:
-   uint32 tickCountCurrent;
-   uint32 tickCountNext;
-   int64 perfCountCurrent;
-   int64 perfCountNext;
-   int64 frequency;
-   double perfCountRemainderCurrent;
-   double perfCountRemainderNext;
-   bool usingPerfCounter;
+       // get the current ticks or time relative to the initial time
+       int64 GetTicks();
    
-public:
-    Win32Timer();
-
-    // get the current ticks or time relative to the initial time
-    int64 GetTicks();
-
-    const int32 GetMilliSecs();
-
-    const double GetSeconds();
-
-    // get the time for the specified number of ticks
-    const double GetSeconds( const int64 numTicks ) const;
-
-    // get the number of ticks for the specified time
-    int64 GetTicks( const double seconds ) const;
-
-    // reset so that initial ticks is the current time
-    void Reset();
-};
+       const int32 GetMilliSecs();
+   
+       const double GetSeconds();
+   
+       // reset so that initial ticks is the current time
+       void Reset();
+   };
+   
+   // high-resolution timer (64-bit)
+   class Win32Timer64
+   {
+   private:
+      int64 m_perfCountCurrent;
+      int64 m_perfCountNext;
+      int64 m_frequency;
+      double m_perfCountRemainderCurrent;
+      double m_perfCountRemainderNext;
+      bool m_usingPerfCounter;
+      
+   public:
+      // get the time for the specified number of ticks
+      const double GetSeconds( const int64 numTicks ) const;
+   
+      // get the number of ticks for the specified time
+      int64 GetTicks( const double seconds ) const;
+   };
 
 } // namespace win32timer
 
